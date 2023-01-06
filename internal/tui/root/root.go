252d890b73fc7b91ad9075b27bf846a88f573114
vk <kotliarvladyslav@gmail.com>
2023-01-06 15:49:10 +0200

    simplify root view

--- internal/tui/root/root.go
+++ internal/tui/root/root.go
@@ -1,56 +1,87 @@
 package root
 
 import (
+	"fmt"
+
 	"github.com/gdamore/tcell/v2"
+	"github.com/kotlw/gentlemoney/internal/presenter"
+	"github.com/kotlw/gentlemoney/internal/service"
+	"github.com/kotlw/gentlemoney/internal/tui/transactions"
+
 	"github.com/rivo/tview"
 )
 
-// Root is the root view of the app. It aggregates pages and navBar in flex.
+// Root is the root view of the app. It aggregates pages and navbar in flex.
 type Root struct {
 	*tview.Flex
 
-	navBar   *navBar
-	pages    *tview.Pages
-	captures []func(*tcell.EventKey)
+	navbar       *tview.TextView
+	pages        *tview.Pages
+	transactions *transactions.View
 }
 
 // New returns Root.
-func New() *Root {
+func New(service *service.Service, presenter *presenter.Presenter) *Root {
 	root := &Root{
-		Flex:   tview.NewFlex().SetDirection(tview.FlexRow),
-		navBar: newNavBar(),
-		pages:  tview.NewPages(),
+		Flex: tview.NewFlex().SetDirection(tview.FlexRow),
+		navbar: tview.NewTextView().
+			SetDynamicColors(true).
+			SetRegions(true).
+			SetWrap(false),
+		pages: tview.NewPages(),
 	}
 
-	root.AddItem(root.navBar, 1, 1, false)
+	root.AddItem(root.navbar, 1, 1, false)
 	root.AddItem(root.pages, 0, 16, true)
+
+	root.transactions = transactions.New(service, presenter)
+	root.AddView('1', "Transactions", root.transactions)
+
+	root.SwitchToView("Transactions")
 
 	return root
 }
 
-// AddPage adds page with corresponding navbar item.
-func (r *Root) AddView(key rune, name string, view tview.Primitive) {
-	r.navBar.addItem(name, key)
-	r.pages.AddPage(name, view, true, true)
-
-	r.captures = append(r.captures, func(event *tcell.EventKey) {
-		if event.Rune() == key {
-			r.SwitchToView(name)
-		}
-	})
+// AddView adds view with corresponding navbar item.
+func (r *Root) AddView(hint rune, title string, view tview.Primitive) {
+	fmt.Fprintf(r.navbar, `  ["%s"]%c %s[""] `, title, hint, title)
+	r.pages.AddPage(title, view, true, false)
 }
 
 // SwitchToView switches to page by name and highlights corresponting navbar item.
-func (l *Root) SwitchToView(name string) {
-	l.navBar.highlightItem(name)
-	l.pages.SwitchToPage(name)
+func (r *Root) SwitchToView(name string) {
+	r.navbar.Highlight(name)
+	r.navbar.ScrollToHighlight()
+	r.pages.SwitchToPage(name)
 }
 
-// Capture is a func with defined conditions to switch to certain page by defined key.
-// Should be used with SetInputCapture func.
-func (l *Root) Capture(event *tcell.EventKey) *tcell.EventKey {
-	for _, cap := range l.captures {
-		cap(event)
-	}
-	return event
+// IsModalOnTop check if modal of any child view is on top.
+func (r *Root) IsModalOnTop() bool {
+	return r.transactions.ModalHasFocus()
+}
+
+// InputHandler returns the handler for this primitive.
+func (r *Root) InputHandler() func(event *tcell.EventKey, setFocus func(p tview.Primitive)) {
+	return r.WrapInputHandler(func(event *tcell.EventKey, setFocus func(p tview.Primitive)) {
+		// modal shouldn't be on top, to perform switch view.
+		if !r.IsModalOnTop() {
+			if event.Rune() == '1' {
+				r.SwitchToView("Transactions")
+
+				return
+			}
+		}
+
+		// if modal is active all other handlers should be ignored except modal handler.
+		for _, view := range []tview.Primitive{r.transactions} {
+			if view.HasFocus() {
+				// give control to the child view.
+				if handler := view.InputHandler(); handler != nil {
+					handler(event, setFocus)
+
+					return
+				}
+			}
+		}
+	})
 }
--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -4,25 +4,12 @@ import (
 	"github.com/kotlw/gentlemoney/internal/presenter"
 	"github.com/kotlw/gentlemoney/internal/service"
 	"github.com/kotlw/gentlemoney/internal/tui/root"
-	"github.com/kotlw/gentlemoney/internal/tui/transactions"
 
-	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func New(service *service.Service, presenter *presenter.Presenter) *tview.Application {
-	root := root.New()
-	transactionsView := transactions.New(service, presenter)
-	root.AddView('1', "Transactions", transactionsView)
-	root.AddView('2', "Settings", tview.NewBox())
-	root.SwitchToView("Transactions")
-
-	root.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		root.Capture(event)
-
-		return event
-	})
-
+	root := root.New(service, presenter)
 	app := tview.NewApplication()
 	app.SetRoot(root, true).EnableMouse(false)
 
